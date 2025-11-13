373fe06cc53dc136ee09dd1fc6bdaece4b5fbdd3
milad.esfandiary <miladesfandiary1990@gmail.com>
2025-11-13 18:12:42 +0330

    add visualizer method

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from math import sin
-import numpy as np
+import matplotlib.pyplot as plt
 class Signal:
     def __init__(self):
         pass    
-    def generate(self, frequency,sample=1000):
-        return(np.sin(frequency*np.array(range(sample))))
+    def generate(self, frequency):
+        return([1,2,3])
     def visualize (self):
-        pass
+        plt.plot(x, y)
     def add_noise(self):
         pass
-s1=Signal()
-print(s1.generate(10))
