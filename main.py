ada985553eea0d5ef2d3c20340ea3e5f5427d870
mrparsaeian <m.r.parsa@gmail.com>
2025-11-13 17:27:45 +0330

    signal class struct #3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ class Signal:
     def __init__(self):
         pass    
     def generate(self, frequency):
-        pass
+        return([1,2,3])
     def visualize (self):
         pass
     def add_noise(self):
