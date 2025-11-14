efb5eda6879200d97e0cdb9ef951b9e2367762dc
milad.esfandiary <miladesfandiary1990@gmail.com>
2025-11-14 09:36:58 +0330

    add plot

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
-from math import sin
-import numpy as np
-
-
+import matplotlib.pyplot as plt
 class Signal:
     def __init__(self):
-        pass
-
-    def generate(self, frequency, duration, amplitude, sample_rate):
-        return np.sin(frequency * np.array(range(sample_rate * duration)))
-
-    def visualize(self):
-        pass
-
+        pass    
+    def generate(self, frequency):
+        return([1,2,3])
+    def visualize (self):
+        plt.plot(x, y)
     def add_noise(self):
         pass
-
-
-s1 = Signal()
-print(s1.generate(10))
