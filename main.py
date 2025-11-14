0c2fae5120eb68d45ac13eef2785b156775dd782
milad.esfandiary <miladesfandiary1990@gmail.com>
2025-11-14 11:40:20 +0330

    add structure

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-from math import sin
-import numpy as np
-
-
 class Signal:
     def __init__(self, frequency, amplitude, duration, sample_rate):
-        self.frequency = frequency
-        self.amplitude = amplitude
-        self.duration = duration
-        self.sample_rate = sample_rate
-        self.signal= generate(frequency)  
+
     def generate(self, frequency, duration, amplitude, sample_rate):
-        return np.sin(frequency * np.array(range(sample_rate * duration)))
 
     def visualize(self, x, y):
-        plt.plot(x, y)
-        plt.show()
+ 
     def add_noise(self):
-        pass
 
 
 s1 = Signal()
