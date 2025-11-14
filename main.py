d6b79984854120b5e52664d5f5d4e6573118acc1
Amirhosein.Pourabdollah <pourabdollahamirhosein@gmail.com>
2025-11-14 16:19:06 +0330

    final version

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Signal:
         plt.ylabel("Amplitude")
         plt.show()
 
-    def add_noise(self, noise_level=0.3):
+    def add_noise(self, noise_level=0.1):
         noise = noise_level * np.random.randn(len(self.signal))
         self.signal = self.signal + noise
 
@@ -43,16 +43,18 @@ class Signal:
 
 
 # Example usage
-s1 = Signal(frequency=10, amplitude=1, duration=1, sample_rate=1000)
+f1=int(input())
+s1 = Signal(frequency=f1, amplitude=1, duration=1, sample_rate=1000)
+print (s1.signal)
+s1.visualize()
 s1.add_noise()
-
+s1.visualize()
 s2 = Signal(frequency=15, amplitude=1, duration=1, sample_rate=1000)
 s2.add_noise()
 s3 = Signal(frequency=5, amplitude=1, duration=1, sample_rate=1000)
-s4 = Signal(frequency=2, amplitude=1, duration=1, sample_rate=1000)
 
 
 s3.add_noise()
 # Now works:
-s5 = s1 + s2 + s3 +s4
+s5 = s1 + s2 + s3
 s5.visualize()
