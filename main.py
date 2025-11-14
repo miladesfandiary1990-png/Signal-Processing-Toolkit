d6af121c87c69130a337e9776f841229bc40912a
mrparsaeian <m.r.parsa@gmail.com>
2025-11-14 09:24:02 +0330

    push to branch generator signal generator #3

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from math import sin
 import numpy as np
+
+
 class Signal:
     def __init__(self):
-        pass    
-    def generate(self, frequency,sample=1000):
-        return(np.sin(frequency*np.array(range(sample))))
-    def visualize (self):
         pass
+
+    def generate(self, frequency, duration, amplitude, sample_rate):
+        return np.sin(frequency * np.array(range(sample_rate * duration)))
+
+    def visualize(self):
+        pass
+
     def add_noise(self):
         pass
-s1=Signal()
+
+
+s1 = Signal()
 print(s1.generate(10))
