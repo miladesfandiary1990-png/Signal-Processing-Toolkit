38ecaf81286bf1f19ff232faed8508fdef55ea5e
milad.esfandiary <miladesfandiary1990@gmail.com>
2025-11-14 15:18:32 +0330

    revise visualize

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 class Signal:
-    def visualize(self, x, y):
-        plt.plot(x, y)
-        plt.show()
+    def visualize(self, signal):
+        fig, axs = plt.subplots(1, 3, figsize=(10, 8))
+        for i in signal in enumerate(signals):
+        x = range(len(signal))
+        axs[i].plot(x. signal)
+        axs[i].set_title(f'signal {signal(i).frequency} Hz')
+        axs[i].grid(True)
+
+    plt.tight_layout()
+    plt.show()
