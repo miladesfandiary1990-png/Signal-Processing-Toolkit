0f6f3a80806914bf009338ed0bc04431a4bd320f
mrparsaeian <m.r.parsa@gmail.com>
2025-11-14 09:43:25 +0330

    push to branch generator signal generator #3

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import matplotlib.pyplot as plt
+from math import sin
+import numpy as np
+
+
 class Signal:
     def __init__(self):
-        pass    
-    def generate(self, frequency):
-        return([1,2,3])
-    def visualize (self):
-        plt.plot(x, y)
+        pass
+
+    def generate(self, frequency, duration, amplitude, sample_rate):
+        return np.sin(frequency * np.array(range(sample_rate * duration)))
+
+    def visualize(self):
+        pass
+
     def add_noise(self):
         pass
+
+
+s1 = Signal()
+print(s1.generate(10))
